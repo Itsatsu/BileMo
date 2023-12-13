285a9f5280253dc792c114ca1505734d55db5979
Eric <e.gigondan@gmail.com>
2023-12-13 16:38:38 +0100

    update phone controller

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api/phones')]
 
 class PhoneController extends AbstractController
 {
-    #[Route('/phone', name: 'app_phone')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'app_phone',methods: ['GET'])]
+    public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneController.php',
-        ]);
+        $phoneList = $phoneRepository->findAll();
+        $jsonPhoneList = $serializer->serialize($phoneList, 'json', ['groups' => 'getPhones']);
+        return new JsonResponse(
+            $jsonPhoneList, Response::HTTP_OK, [], true
+        );
+    }
+
+    #[Route('/{id}', name: 'app_phone_detail', methods: ['GET'])]
+    public function getPhone(Phone $phone, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonPhone = $serializer->serialize($phone, 'json', ['groups' => 'getPhoneDetail']);
+        return new JsonResponse(
+            $phone, Response::HTTP_OK, [], true
+        );
     }
 }
