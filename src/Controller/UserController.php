d59008a2d0d865716b14b642e49f9e4a1b6e623e
Eric <e.gigondan@gmail.com>
2023-12-14 18:06:51 +0100

    Update with delete cache customer when user DELETE/PUT/POST

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -37,10 +37,12 @@ class UserController extends AbstractController
     }
 
     #[Route('api/users/{id}', name: 'user_delete', methods: ['DELETE'])]
-    public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse
+    public function deleteUser(User $user, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse
     {
+        $cache->invalidateTags(['usersCache' . $user->getCustomer()->getId()]);
         $em->remove($user);
         $em->flush();
+
         return new JsonResponse(
             null,
             Response::HTTP_NO_CONTENT
@@ -49,7 +51,7 @@ class UserController extends AbstractController
     }
 
     #[Route('api/users/{id}', name: 'user_update', methods: ['PUT'])]
-    public function updateUser(User $user, SerializerInterface $serializer, EntityManagerInterface $em, Request $request, CustomerRepository $customerRepository, ValidatorInterface $validator): JsonResponse
+    public function updateUser(User $user, SerializerInterface $serializer, EntityManagerInterface $em, Request $request, CustomerRepository $customerRepository, ValidatorInterface $validator, TagAwareCacheInterface $cache, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $updatedUser = $serializer->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $user]);
         $errors = $validator->validate($updatedUser);
@@ -63,9 +65,33 @@ class UserController extends AbstractController
             );
         }
 
+        //On récupère l'id du customer avant la modification et on invalide le cache
+        $customer = $user->getCustomer()->getId() ?? null;
+        if ($customer !== null) {
+            $cache->invalidateTags(['usersCache' . $user->getCustomer()->getId()]);
+        }
+
         $content = $request->toArray();
         $idCustomer = $content['customerId'] ?? null;
-        $updatedUser->setCustomer($customerRepository->find($idCustomer));
+        $password = $content['password'] ?? null;
+
+        //mot de passe modifié
+        if ($password !== null) {
+            $updatedUser->setPassword(
+                $passwordHasher->hashPassword(
+                    $updatedUser,
+                    $content['password']
+                )
+            );
+        }
+
+        //Customer modifié
+        if ($idCustomer !== null) {
+            $newCustomer = $customerRepository->find($idCustomer);
+            $updatedUser->setCustomer($newCustomer);
+        }
+
+        $cache->invalidateTags(['usersCache' . $updatedUser->getCustomer()->getId()]);
 
         $em->persist($updatedUser);
         $em->flush();
@@ -78,7 +104,7 @@ class UserController extends AbstractController
     }
 
     #[Route('api/users', name: 'user_create', methods: ['POST'])]
-    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, CustomerRepository $customerRepository, ValidatorInterface $validator, UrlGeneratorInterface $urlGenerator): JsonResponse
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, CustomerRepository $customerRepository, ValidatorInterface $validator, UrlGeneratorInterface $urlGenerator, TagAwareCacheInterface $cache): JsonResponse
     {
 
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
@@ -104,6 +130,7 @@ class UserController extends AbstractController
         $user->setRoles(['ROLE_USER']);
         $em->persist($user);
         $em->flush();
+        $cache->invalidateTags(['usersCache' . $user->getCustomer()->getId()]);
         $location = $urlGenerator->generate('app_user_detail', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUserDetail']);
 
@@ -124,7 +151,8 @@ class UserController extends AbstractController
         $idCache = 'customer-users-list-' . $customer->getId() . '-' . $page . '-' . $limit;
 
         $jsonUsers = $cache->get($idCache, function (ItemInterface $item) use ($userRepository, $customer, $page, $limit, $serializer) {
-            $item->tag('usersCache');
+            echo 'No cache';
+            $item->tag('usersCache' . $customer->getId());
             $users = $userRepository->findByCustomerUserPagined($customer->getId(), $page, $limit);
             return $serializer->serialize($users, 'json', ['groups' => 'getUserDetail']);
         });
